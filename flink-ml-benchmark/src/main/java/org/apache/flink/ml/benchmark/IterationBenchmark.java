2307c6e9194cd8917d7181a9867d7cab1c934e4e
yunfengzhou-hub <yuri.zhouyunfeng@outlook.com>
2022-06-17 15:12:12 +0800

    refractor iteration body

--- flink-ml-benchmark/src/main/java/org/apache/flink/ml/benchmark/IterationBenchmark.java
+++ flink-ml-benchmark/src/main/java/org/apache/flink/ml/benchmark/IterationBenchmark.java
@@ -23,11 +23,17 @@ import org.apache.flink.iteration.DataStreamList;
 import org.apache.flink.iteration.IterationBody;
 import org.apache.flink.iteration.IterationBodyResult;
 import org.apache.flink.iteration.IterationConfig;
+import org.apache.flink.iteration.IterationListener;
 import org.apache.flink.iteration.Iterations;
 import org.apache.flink.iteration.ReplayableDataStreamList;
+import org.apache.flink.ml.common.iteration.ForwardInputsOfLastRound;
 import org.apache.flink.ml.common.iteration.TerminateOnMaxIter;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.operators.AbstractStreamOperator;
+import org.apache.flink.streaming.api.operators.OneInputStreamOperator;
+import org.apache.flink.streaming.runtime.streamrecord.StreamRecord;
+import org.apache.flink.util.Collector;
 import org.apache.flink.util.NumberSequenceIterator;
 
 import org.apache.commons.cli.CommandLine;
@@ -38,6 +44,7 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.collections.IteratorUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /** Example code to test the performance of flink-ml-iteration. */
@@ -61,40 +68,68 @@ public class IterationBenchmark {
     private static final Options OPTIONS =
             new Options().addOption(ITER_OPTION).addOption(REPEAT_OPTION);
 
-    public static void main(String[] args) throws Exception {
+    private static List<Long> executeAndCollect(StreamExecutionEnvironment env, int maxIter)
+            throws Exception {
+        DataStream<Long> stream =
+                env.fromParallelCollection(
+                        new NumberSequenceIterator(1L, 10L), BasicTypeInfo.LONG_TYPE_INFO);
+
+        IterationConfig config =
+                IterationConfig.newBuilder()
+                        .setOperatorLifeCycle(IterationConfig.OperatorLifeCycle.ALL_ROUND)
+                        .build();
+
+        IterationBody iterationBody = new NoOpIterationBody(maxIter);
+
+        DataStream<Long> output =
+                Iterations.iterateBoundedStreamsUntilTermination(
+                                DataStreamList.of(stream),
+                                ReplayableDataStreamList.notReplay(stream),
+                                config,
+                                iterationBody)
+                        .get(0);
+
+        return IteratorUtils.toList(output.executeAndCollect());
+    }
+
+    private static void executeWithArgs(String[] args) throws Exception {
         CommandLineParser parser = new DefaultParser();
         CommandLine commandLine = parser.parse(OPTIONS, args);
 
         int maxIter = Integer.parseInt(commandLine.getOptionValue(ITER_OPTION));
         int repeat = Integer.parseInt(commandLine.getOptionValue(REPEAT_OPTION));
 
+        System.out.println("iteration: " + maxIter + " repeat: " + repeat);
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setBufferTimeout(10);
 
         for (int i = 0; i < repeat; i++) {
-            DataStream<Long> stream =
-                    env.fromParallelCollection(
-                            new NumberSequenceIterator(1L, 10L), BasicTypeInfo.LONG_TYPE_INFO);
-
-            IterationConfig config =
-                    IterationConfig.newBuilder()
-                            .setOperatorLifeCycle(IterationConfig.OperatorLifeCycle.ALL_ROUND)
-                            .build();
-
-            IterationBody iterationBody = new NoOpIterationBody(maxIter);
-
-            DataStream<Long> output =
-                    Iterations.iterateBoundedStreamsUntilTermination(
-                                    DataStreamList.of(stream),
-                                    ReplayableDataStreamList.notReplay(stream),
-                                    config,
-                                    iterationBody)
-                            .get(0);
-
             long startTime = System.currentTimeMillis();
-            if (IteratorUtils.toList(output.executeAndCollect()).size() != 10L) {
-                throw new RuntimeException();
-            }
+            System.out.println(executeAndCollect(env, maxIter));
             System.out.println(System.currentTimeMillis() - startTime);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            executeWithArgs(args);
+        }
+
+        List<Integer> iterList = Arrays.asList(10, 20, 50, 100, 200, 500);
+        List<Integer> timeoutList = Arrays.asList(0, 1, 10, 100);
+
+        for (int bufferTimeout : timeoutList) {
+            StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+            env.setBufferTimeout(bufferTimeout);
+
+            for (int maxIter : iterList) {
+                long startTime = System.currentTimeMillis();
+                executeAndCollect(env, maxIter);
+                System.out.printf(
+                        "Timeout: %d ms\t MaxIter: %d \t Time: %d ms\n",
+                        bufferTimeout, maxIter, System.currentTimeMillis() - startTime);
+            }
         }
     }
 
@@ -111,20 +146,45 @@ public class IterationBenchmark {
             DataStream<Integer> terminationCriteria =
                     variableStreams.get(0).flatMap(new TerminateOnMaxIter<>(maxIterationNum));
 
-            List<DataStream<?>> variableList = new ArrayList<>();
-            for (DataStream<?> stream : variableStreams.getDataStreams()) {
-                variableList.add(stream.map(x -> x));
-            }
-
+            List<DataStream<Long>> variableList = new ArrayList<>();
             List<DataStream<?>> dataList = new ArrayList<>();
-            for (DataStream<?> stream : dataStreams.getDataStreams()) {
-                dataList.add(stream.map(x -> x));
+            for (int i = 0; i < variableStreams.size(); i++) {
+                DataStream<Long> stream = variableStreams.get(i);
+                stream =
+                        stream.transform(
+                                "AddOneOperator",
+                                BasicTypeInfo.LONG_TYPE_INFO,
+                                new AddOneOperator());
+                variableList.add(stream);
+                dataList.add(stream.flatMap(new ForwardInputsOfLastRound<>()));
             }
 
             return new IterationBodyResult(
                     DataStreamList.of(variableList.toArray(new DataStream<?>[0])),
                     DataStreamList.of(dataList.toArray(new DataStream<?>[0])),
                     terminationCriteria);
+        }
+    }
+
+    private static class AddOneOperator extends AbstractStreamOperator<Long>
+            implements OneInputStreamOperator<Long, Long>, IterationListener<Long> {
+        private final List<Long> list = new ArrayList<>();
+
+        @Override
+        public void onEpochWatermarkIncremented(
+                int epochWatermark, Context context, Collector<Long> collector) {
+            for (Long x : list) {
+                collector.collect(x + 1);
+            }
+            list.clear();
+        }
+
+        @Override
+        public void onIterationTerminated(Context context, Collector<Long> collector) {}
+
+        @Override
+        public void processElement(StreamRecord<Long> streamRecord) {
+            list.add(streamRecord.getValue());
         }
     }
 }
