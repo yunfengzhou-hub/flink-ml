9c8bd5e891f459e4febb8cbabcd951a0ba087290
yunfengzhou-hub <yuri.zhouyunfeng@outlook.com>
2022-06-28 08:58:42 +0800

    tmp

--- flink-ml-python/pyflink/ml/lib/evaluation/tests/test_binaryclassificationevaluator.py
+++ flink-ml-python/pyflink/ml/lib/evaluation/tests/test_binaryclassificationevaluator.py
@@ -15,6 +15,7 @@
 #  See the License for the specific language governing permissions and
 # limitations under the License.
 ################################################################################
+import os
 
 from pyflink.common import Types
 from pyflink.ml.core.linalg import Vectors, DenseVectorTypeInfo
